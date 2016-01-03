fa54b150dcc22d85bd2b94cea0b23f1ba800379b
linbiqiu <741671416@qq.com>
2016-01-03 16:13:44 +0800

    Login

--- schoolassist/src/main/java/com/nima519/ui/LoginActivity.java
+++ schoolassist/src/main/java/com/nima519/ui/LoginActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 
 import com.example.schoolassist.R;
 import com.nima519.ui.base.BaseActivity;
+import com.nima519.utils.Content;
+import com.nima519.utils.L;
+
+import cn.bmob.v3.Bmob;
 
 public class LoginActivity extends BaseActivity {
 
@@ -11,6 +15,7 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        Bmob.initialize(this, Content.ApplicationID);
+        L.i(TAG,TAG+"has launched !!!!!");
     }
 }
